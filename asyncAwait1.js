36a3e02fb1cd37cd89aef5c4c5870e620f3ca86d
Utkarsh Harshe <utkarshj0119@gmail.com>
2025-02-17 00:15:39 +0530

    Js prep loading...

--- asyncAwait1.js
+++ asyncAwait1.js
@@ -56,4 +56,67 @@ async function getRandomUserData() {
     }
 }
 
-getRandomUserData();
+// getRandomUserData();
+
+/*
+3) Fetching Data from API:
+Fetch data from https://jsonplaceholder.typicode.com/todos/1 using fetch and await. 
+Print the response in a structured format.
+*/
+
+async function fechDataAPI() {
+    try {
+        let res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        let data = await res.json();
+        console.log("data", data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+// fechDataAPI()
+
+
+/*
+4) Multiple API Calls (Promise.all):
+Fetch data from two different endpoints:
+https://jsonplaceholder.typicode.com/users/1
+https://jsonplaceholder.typicode.com/posts/1
+Use Promise.all to execute both requests concurrently and display the results.
+*/
+
+async function allData() {
+    try {
+        let [user,post] = await Promise.all([
+            fetch("https://jsonplaceholder.typicode.com/users/1").then(res=>res.json()),
+            fetch("https://jsonplaceholder.typicode.com/posts/1").then(res=>res.json())
+        ])
+        console.log("User", user);
+        console.log("Post", post);
+    } catch (error) {
+        console.log("Error : ",error)
+    }
+}
+
+// allData();
+
+/*
+5) Chaining Async Calls:
+First, fetch user details from https://jsonplaceholder.typicode.com/users/1. Then, using the id from the user data, 
+fetch the first post of that user from https://jsonplaceholder.typicode.com/posts?userId=1.
+*/
+
+async function userDataFetch() {
+    try {
+        let userRes = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        let user = await userRes.json();
+        
+        let postRes = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`);
+        let post = await postRes.json();
+
+        console.log("User : ",user)
+        console.log("User first post : ",post[0])
+    } catch (error) {
+        console.log(error)
+    }
+}
+userDataFetch()
